8fc97a05ccd80d32b579e5140dd72fa36aa565ff
Matt Farmer <mrfarmer777@gmail.com>
2018-06-18 01:57:58 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
   def change
-    create_table :songs do |t|
+    create_table :artists do |t|
       t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
     end
   end
 end
